0e0bfaf3c3a95183c518a02b7264d3b340ef6f05
Xiao Yong <570057644@qq.com>
2018-03-15 19:09:10 +0800

    优化邮件界面

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,1 +1,23 @@
-请点击链接{{env('APP_URL')}}/mail/leave/{{$code}}
+<p>
+    <strong>你好：</strong>
+</p>
+<blockquote>
+    <p>
+        <strong>你部门现有助理上传电子假条，请及时登录系统进行审核。</strong>
+    </p>
+    <p>
+        <strong>点击下列链接可进行快速审核：</strong>
+    </p>
+    <p>
+        <strong>{{env('APP_URL')}}/mail/leave/{{$code}}</strong>
+    </p>
+    <p>
+        <strong>若有任何疑问，请联系客服人员进行处理。</strong>
+    </p>
+</blockquote>
+<p style="text-align:right;">
+    <strong>西华师范大学易班OA系统</strong>
+</p>
+<p>
+    <strong>系统邮件，请勿回复。</strong>
+</p>
